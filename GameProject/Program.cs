a8d70821a1ff3bfe8bef3a82454de11ad484a236
win7 <win7@BILGISAYAR>
2021-01-29 19:24:43 +0300

    game choices were added

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -32,11 +32,11 @@ namespace GameProject
             gameManager.GameCampaign(game1, campaign1);
             gameManager.GameOrder(game1, gamer2);
             Game game2 = new Game { GameName = "Zuma", GamePrice = 0 };
+            Game game3 = new Game { GameName = "Sudoku", GamePrice = 30 };
+            Game game4 = new Game { GameName = "Word Game", GamePrice = 15 };
 
-        
         Decision:
             Console.WriteLine("Do you want to get free 'Zuma' game: Y or N");
-        
             string userdecision = (Console.ReadLine().ToUpper());
             switch (userdecision)
             {
@@ -45,14 +45,46 @@ namespace GameProject
                     goto Start;
                 case "N":
                 Start:
-                    Console.WriteLine("Go to the menu for other games.");
+                NewGame:
+                    Console.WriteLine("Do you want to get a new game: Y or N");
+                    string userdecision2 = (Console.ReadLine().ToUpper());
+                    switch (userdecision2)
+                    {
+                        case "Y":
+                        StartGame:
+                        
+                            Console.WriteLine("PLease choose a new game: 1 - Snake, 2 - Sudoku, 3 - Word Game");
+                            int UserChoice = int.Parse(Console.ReadLine());
+                            switch (UserChoice)
+                            {
+                                case 1:
+                                    gameManager.GameOrder(game1, gamer2);
+                                    break;
+                                case 2:
+                                    gameManager.GameOrder(game3, gamer2);
+                                    break;
+                                case 3:
+                                    gameManager.GameOrder(game4, gamer2);
+                                    break;
+                                default:
+                                    Console.WriteLine("your choice is invalid");
+                                    goto StartGame;
+                            }
+                            goto Start;
+                        case "N":
+                            Console.WriteLine("Thanks");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice.");
+                            goto NewGame;
+                    }
                     break;
                 default:
                     Console.WriteLine("Invalid choice.");
                     goto Decision;
             }
-        
-            
+           
+
 
         }
     }
